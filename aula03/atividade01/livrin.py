5b2727b81eef70dc3ca00c4eebaf41e0e859488d
Alexiafern <alexiafernanda718@gmail.com>
2023-11-14 10:12:02 -0300

    Batatinha

--- aula03/atividade01/livrin.py
+++ aula03/atividade01/livrin.py
@@ -1,8 +1,13 @@
 class livro:
     def __init__(self, titulo, autor, ano_publicacao) -> None:
-        self.titulo = 'Batata'
-        self.autor = "Batatinha"
-        self.ano_publicacao = 2023
-    def descricao():
-        return str.format()
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_publicacao = ano_publicacao
+    def descricao(self):
+        return f"Titulo:  {self.titulo}, autor: {self.autor}, ano de publicaçao: {self.ano_publicacao}"
+livro1 = livro("Batata", "batatinha", 2023)
+livro2 = livro("atata", "taata", 2023)
+
+print(livro1.descricao())
+print(livro2.descricao())
     
